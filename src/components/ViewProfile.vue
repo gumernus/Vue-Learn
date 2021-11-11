01045bcb93709b9766e5b85986bd30c3d3ff838a
Michal Kopčík <70224036+gumernus@users.noreply.github.com>
2021-11-11 19:05:59 +0100

    Create ViewProfile.vue

---
+++ src/components/ViewProfile.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="view-profile">
+<h1>{{ userId }}</h1>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'ViewProfile',
+  data() {
+      return {
+          userId: this.$route.params.user_id
+      }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
