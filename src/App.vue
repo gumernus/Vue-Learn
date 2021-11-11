8b8c6df93ae01d7560a6baeb4612b2f68566f107
Michal Kopčík <70224036+gumernus@users.noreply.github.com>
2021-11-11 18:13:47 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
 <template>
-  <div id="nav">
-    <router-link :to="{name: 'Home'}">Home</router-link> |
-    <router-link :to="{name: 'About'}">About</router-link> |
-    <router-link :to="{name: 'Profile'}">Profile</router-link>
+  <nav id="nav" class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Gumernus</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#"><router-link :to="{name: 'Home'}">Home</router-link></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><router-link :to="{name: 'About'}">About</router-link></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><router-link :to="{name: 'Profile'}">Profile</router-link></a>
+        </li>
+      </ul>
+    </div>
   </div>
+</nav>
   <router-view />
 </template>
 
@@ -12,17 +28,15 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
-#nav {
-  font-size: 30px;
-  padding: 30px;
+h1{
+  padding: 10px;
+  font-weight: bold;
 }
 
 #nav a {
-  font-weight: bold;
   color: #2c3e50;
   text-decoration: none;
 }
