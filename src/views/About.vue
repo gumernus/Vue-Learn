2b31ecd2f5eeb13fb39457d16e69898dbf28d169
Michal Kopčík <70224036+gumernus@users.noreply.github.com>
2021-11-12 18:42:14 +0100

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,24 +1,43 @@
 <template>
-<h1 v-if="!result">no response</h1>
-<h1 v-else>{{result}}</h1>
+  <h1 v-if="!result">
+    <div class="placeholder-item"><br /></div>
+    <br>
+    <div class="placeholder-item"><br /><br /></div>
+  </h1>
+
+  <h1 v-else>
+    <div class="placeholder-item">{{ result.chartName }}</div>
+    <br />
+    <div class="placeholder-item">{{ result.disclaimer }}</div>
+  </h1>
+
+  <!-- <h1>{{result.disclaimer}}</h1> -->
 </template>
 
 <script>
 import axios from "axios";
 export default {
-  name: 'Template',
-data () {
+  name: "Template",
+  data() {
     return {
-      result: null
-    }
+      result: null,
+    };
   },
-  mounted () {
+  mounted() {
     axios
-      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
-      .then(response => (this.result = response.data.disclaimer))
-  }
+      .get("https://api.coindesk.com/v1/bpi/currentprice.json")
+      .then((response) => (this.result = response.data));
+  },
 };
 </script>
 
 <style scoped>
+.placeholder-item {
+  box-shadow: 0 4px 10px 0 rgba(33, 33, 33, 0.15);
+  border-radius: 20px;
+  padding: 20px;
+  position: relative;
+  overflow: hidden;
+  background-color: #fff;
+}
 </style>
