b259504a3a1574e9837bda6beaebc04fc2b1a8ec
Michal Kopčík <70224036+gumernus@users.noreply.github.com>
2021-11-12 17:52:52 +0100

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="about">
-    <h1>This is about bcs in routes is path: "/about" </h1>
-  </div>
+<h1 v-if="count === 0">Vue is awesome!</h1>
+<h1 v-else>Oh no 😢</h1>
 </template>
+
+<script>
+export default {
+  name: 'Template',
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
